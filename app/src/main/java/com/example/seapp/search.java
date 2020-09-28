4b7916be9d031e6faa3e3c936502197691ede0b8
DESKTOP-DR4T9QF\kmang <katelyn.mangold@my.simpson.edu>
2020-09-28 15:07:27 -0500

    1

--- app/src/main/java/com/example/seapp/search.java
+++ app/src/main/java/com/example/seapp/search.java
@@ -1,14 +1,21 @@
 package com.example.seapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.SearchView;
+import android.widget.Toolbar;
+
+import com.google.android.material.tabs.TabLayout;
 
 import java.util.ArrayList;
 
@@ -19,6 +26,6 @@ public class search extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
 
-
         }
+
 }
