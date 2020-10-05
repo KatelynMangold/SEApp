b79d40ac96347dd9a935b50f73d1b96fff70d8dc
DESKTOP-DR4T9QF\kmang <katelyn.mangold@my.simpson.edu>
2020-10-05 15:05:05 -0500

    3

--- app/src/main/java/com/example/seapp/search.java
+++ app/src/main/java/com/example/seapp/search.java
@@ -2,17 +2,20 @@ package com.example.seapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.SearchView;
 import android.widget.Toolbar;
+import android.view.View;
 
 import com.google.android.material.tabs.TabLayout;
 
@@ -20,11 +23,23 @@ import java.util.ArrayList;
 
 public class search extends AppCompatActivity {
 
-    @Override
+
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
 
+
+        Button homeButton;
+        homeButton = (Button) findViewById(R.id.homeButton);
+        homeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(i);
+            }
+        }); {
+
         }
+    }
 
 }
