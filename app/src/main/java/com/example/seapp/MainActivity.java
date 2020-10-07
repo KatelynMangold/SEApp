b2abf5d512c3133a40f8e5cf605126226421ccfc
bishnuadhikari <bishnu.adhikari@my.simpson.edu>
2020-10-07 15:12:31 -0500

    Bishnu Adhikari seekbar has been added to the progress bar.

--- app/src/main/java/com/example/seapp/MainActivity.java
+++ app/src/main/java/com/example/seapp/MainActivity.java
@@ -7,47 +7,67 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.SeekBar;
+import android.widget.TextView;
 import android.widget.Toast;
 
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+import java.util.Timer;
+import java.util.TimerTask;
 
+public class MainActivity extends AppCompatActivity
+{
+
+    private TextView textView;
+    private ProgressBar progressBar;
+    private SeekBar seekBar;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textView = (TextView)findViewById(R.id.textView);
+        progressBar = (ProgressBar)findViewById(R.id.progressBar);
+        seekBar = (SeekBar)findViewById(R.id.seekBar);
+
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser)
+            {
+                progressBar.setProgress(progress);
+                textView.setText("" + progress + "%");
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar)
+            {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
 
         FloatingActionButton addFood;
         addFood = (FloatingActionButton) findViewById(R.id.addFood);
         addFood.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
+            public void onClick(View v)
+            {
                 startActivity(new Intent(MainActivity.this, search.class));
             }
+
+
         });
 
-        Button incr;
-        Button decr;
-        incr = (Button) findViewById(R.id.button_incr);
-        decr = (Button) findViewById(R.id.button_decr);
-        incr.setOnClickListener(this);
-        decr.setOnClickListener(this);
-
-//        incr.setOnClickListener(new View.OnClickListener()
-//        {
-//            @Override
-//            public void onClick(View v)
-//            {
-//
-//            }
-//        });
-        }
-
-    @Override
-    public void onClick(View v) {
 
     }
+
 }
