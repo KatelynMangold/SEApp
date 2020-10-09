c4029b8041c02be488b95d2c26053e6daaa4953f
DESKTOP-DR4T9QF\kmang <katelyn.mangold@my.simpson.edu>
2020-10-08 21:31:38 -0500

    3

--- app/src/main/java/com/example/seapp/MainActivity.java
+++ app/src/main/java/com/example/seapp/MainActivity.java
@@ -2,23 +2,48 @@ package com.example.seapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.RecyclerView;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.SeekBar;
+import android.widget.TextView;
 import android.widget.Toast;
-
-
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import java.util.Timer;
+import java.util.TimerTask;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
-
+public class MainActivity extends AppCompatActivity {
+    private TextView textView;
+    private ProgressBar progressBar;
+    private SeekBar seekBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        textView = (TextView)findViewById(R.id.textView);
+        progressBar = (ProgressBar)findViewById(R.id.progressBar);
+        seekBar = (SeekBar)findViewById(R.id.seekBar);
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                progressBar.setProgress(progress);
+                textView.setText("" + progress + "%");
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
 
         FloatingActionButton addFood;
         addFood = (FloatingActionButton) findViewById(R.id.addFood);
@@ -28,26 +53,5 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 startActivity(new Intent(MainActivity.this, search.class));
             }
         });
-
-        Button incr;
-        Button decr;
-        incr = (Button) findViewById(R.id.button_incr);
-        decr = (Button) findViewById(R.id.button_decr);
-        incr.setOnClickListener(this);
-        decr.setOnClickListener(this);
-
-//        incr.setOnClickListener(new View.OnClickListener()
-//        {
-//            @Override
-//            public void onClick(View v)
-//            {
-//
-//            }
-//        });
-        }
-
-    @Override
-    public void onClick(View v) {
-
     }
 }
--- app/src/main/java/com/example/seapp/search.java
+++ app/src/main/java/com/example/seapp/search.java
@@ -6,57 +6,44 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
-<<<<<<< HEAD
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
-=======
->>>>>>> c4943b84ec3d8922a4086d40ac7803a83e071c98
+
 import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.SearchView;
-<<<<<<< HEAD
+
 import android.widget.Toolbar;
 import android.view.View;
 
 import com.google.android.material.tabs.TabLayout;
-=======
->>>>>>> c4943b84ec3d8922a4086d40ac7803a83e071c98
 
-import java.util.ArrayList;
 
-public class search extends AppCompatActivity
-{
-
-<<<<<<< HEAD
+public class search extends AppCompatActivity {
+    private Button homeButton;
 
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
 
 
-        Button homeButton;
+
         homeButton = (Button) findViewById(R.id.homeButton);
         homeButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent i = new Intent(getApplicationContext(), MainActivity.class);
-                startActivity(i);
+               openMainActivity();
             }
         }); {
 
         }
     }
-=======
-    @Override
-    public void onCreate(Bundle savedInstanceState)
-    {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_search);
-
->>>>>>> c4943b84ec3d8922a4086d40ac7803a83e071c98
-
+    public void openMainActivity(){
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
     }
+
 }
