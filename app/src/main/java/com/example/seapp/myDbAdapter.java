264629ed0d7a1f9e39025d828b58d49ac17c8143
Mason Spree <mason.spree@my.simpson.edu>
2020-11-13 16:13:16 -0600

    added food log

--- app/src/main/java/com/example/seapp/myDbAdapter.java
+++ app/src/main/java/com/example/seapp/myDbAdapter.java
@@ -4,35 +4,34 @@ import android.content.Context;
 import android.content.ContentValues;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
-import android.database.sqlite.SQLiteOpenHelper;
 
 
-public class myDbAdapter {
-    myDbHelper myhelper;
+public class myDbAdapter{
+    myDBHelper myhelper;
     public myDbAdapter(Context context)
     {
-        myhelper = new myDbHelper(context);
+        myhelper = new myDBHelper(context);
     }
 
     public long insertData(String foodname)
     {
         SQLiteDatabase dbb = myhelper.getWritableDatabase();
         ContentValues contentValues = new ContentValues();
-        contentValues.put(myDbHelper.NAME, foodname);
-        long id = dbb.insert(myDbHelper.TABLE_NAME, null , contentValues);
+        contentValues.put(myDBHelper.NAME, foodname);
+        long id = dbb.insert(myDBHelper.TABLE_NAME, null , contentValues);
         return id;
     }
 
     public String getData()
     {
         SQLiteDatabase db = myhelper.getWritableDatabase();
-        String[] columns = {myDbHelper.UID,myDbHelper.NAME};
-        Cursor cursor =db.query(myDbHelper.TABLE_NAME,columns,null,null,null,null,null);
+        String[] columns = {myDBHelper.UID, myDBHelper.NAME};
+        Cursor cursor =db.query(myDBHelper.TABLE_NAME,columns,null,null,null,null,null);
         StringBuffer buffer= new StringBuffer();
         while (cursor.moveToNext())
         {
-            int cid =cursor.getInt(cursor.getColumnIndex(myDbHelper.UID));
-            String name =cursor.getString(cursor.getColumnIndex(myDbHelper.NAME));
+            int cid =cursor.getInt(cursor.getColumnIndex(myDBHelper.UID));
+            String name =cursor.getString(cursor.getColumnIndex(myDBHelper.NAME));
             buffer.append(cid+ "   " + name +" \n");
         }
         return buffer.toString();
@@ -43,7 +42,7 @@ public class myDbAdapter {
         SQLiteDatabase db = myhelper.getWritableDatabase();
         String[] whereArgs ={uname};
 
-        int count =db.delete(myDbHelper.TABLE_NAME ,myDbHelper.NAME+" = ?",whereArgs);
+        int count =db.delete(myDBHelper.TABLE_NAME , myDBHelper.NAME+" = ?",whereArgs);
         return  count;
     }
 
@@ -51,47 +50,9 @@ public class myDbAdapter {
     {
         SQLiteDatabase db = myhelper.getWritableDatabase();
         ContentValues contentValues = new ContentValues();
-        contentValues.put(myDbHelper.NAME,newName);
+        contentValues.put(myDBHelper.NAME,newName);
         String[] whereArgs= {oldName};
-        int count =db.update(myDbHelper.TABLE_NAME,contentValues, myDbHelper.NAME+" = ?",whereArgs );
+        int count =db.update(myDBHelper.TABLE_NAME,contentValues, myDBHelper.NAME+" = ?",whereArgs );
         return count;
-    }
-
-    static class myDbHelper extends SQLiteOpenHelper {
-        private static final String DATABASE_NAME = "myDatabase";    // Database Name
-        private static final String TABLE_NAME = "myTable";   // Table Name
-        private static final int DATABASE_Version = 1;    // Database Version
-        private static final String UID = "_id";     // Column I (Primary Key)
-        private static final String NAME = "Name";    //Column II
-        private static final String MyPASSWORD = "Password";    // Column III
-        private static final String CREATE_TABLE = "CREATE TABLE " + TABLE_NAME +
-                " (" + UID + " INTEGER PRIMARY KEY AUTOINCREMENT, " + NAME + " VARCHAR(255) ," + MyPASSWORD + " VARCHAR(225));";
-        private static final String DROP_TABLE = "DROP TABLE IF EXISTS " + TABLE_NAME;
-        private Context context;
-
-        public myDbHelper(Context context) {
-            super(context, DATABASE_NAME, null, DATABASE_Version);
-            this.context = context;
-        }
-
-        public void onCreate(SQLiteDatabase db) {
-
-            try {
-                db.execSQL(CREATE_TABLE);
-            } catch (Exception e) {
-                Message.message(context, "" + e);
-            }
-        }
-
-        @Override
-        public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-            try {
-                Message.message(context, "OnUpgrade");
-                db.execSQL(DROP_TABLE);
-                onCreate(db);
-            } catch (Exception e) {
-                Message.message(context, "" + e);
-            }
-        }
     }
 }
